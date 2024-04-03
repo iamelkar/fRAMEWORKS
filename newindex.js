46c2f492d9dc2ae0167c17cea308cfbe9eb7b797
iamelkar <kharhylljoy36@gmail.com>
2024-04-03 20:19:04 +0800

    ejs weather

---
+++ newindex.js
@@ -0,0 +1,53 @@
+
+const weather = require('weather-js');
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+const port = 8000;
+
+app.use((req, res, next) => {
+    console.log('Request made')
+    console.log(`Host: ${req.hostname}`)
+    console.log(`Path: ${req.path}`)
+    console.log(`Method: ${req.method}`)
+    next()
+})
+
+app.get('/', function (req, res) {
+//   res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index', {heading: "I am not throwing away my shot", heading2: "Ayo I'm just like my country I'm young, scrappy, and hungry and I am not throwing away my shot!"})
+})
+
+app.get('/davao', function (req, res) {
+    //   res.sendFile('./views/index.html', {root: __dirname});
+    weather.find({search: 'Davao, PHL', degreeType: 'C'}, function(err, result) {
+        if(err) console.log(err);
+       else{
+        let data = {
+            weatherdvo: eval(JSON.stringify(result, null, 2))
+        }
+        res.render('davao', data)
+       }
+      });
+})
+
+app.get('/about', function (req, res) {
+    res.render('about')
+    // res.sendFile('./views/about.html', {root: __dirname});
+  })
+
+app.get('/home', (req,res) =>{
+    res.redirect('/');
+})
+app.get('/aboutus', (req,res) =>{
+    res.redirect('/about');
+})
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/error.html', {root: __dirname});
+})
